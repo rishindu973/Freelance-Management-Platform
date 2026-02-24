d7d8a9e55f09e0ca1ee0e179c4cd5ee71a9bc4f7
rishindu973 <it24610795@my.sliit.lk>
2026-02-24 06:10:17 +0530

    Initial setup and spring initializing

---
+++ backend/freelancePM/src/main/java/com/freelance/freelancepm/FreelancePmApplication.java
@@ -0,0 +1,13 @@
+package com.freelance.freelancepm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancePmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelancePmApplication.class, args);
+    }
+
+}
