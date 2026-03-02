356163c14e535b611bd09f421a7743e3663265f2
hiruni <it24610786@my.sliit.lk>
2026-03-02 10:30:15 +0530

    Add initial client management project files - Hiruni Pramodini

---
+++ backend/freelancePM/src/main/java/com/freelance/freelancepm/model/Client.java
@@ -0,0 +1,39 @@
+package com.freelance.freelancepm.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
