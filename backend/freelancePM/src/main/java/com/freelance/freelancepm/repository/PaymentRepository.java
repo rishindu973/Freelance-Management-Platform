b73cd99509f3888ca366e4812a3186d82bfe92e1
rishindu973 <IT24610795@my.sliit.lk>
2026-04-03 14:38:17 +0530

    fix previous work and add dto, repository layers

---
+++ backend/freelancePM/src/main/java/com/freelance/freelancepm/repository/PaymentRepository.java
@@ -0,0 +1,13 @@
+package com.freelance.freelancepm.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.freelance.freelancepm.entity.Payment;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+    List<Payment> findByInvoiceId(Integer invoiceId);
+}
