0b5799110198efce3cb6a3c47080e430a5faf67f
rishindu973 <IT24610795@my.sliit.lk>
2026-04-05 17:26:17 +0530

    implement balence tracking

---
+++ backend/freelancePM/src/main/java/com/freelance/freelancepm/dto/PaymentDTO.java
@@ -0,0 +1,17 @@
+package com.freelance.freelancepm.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class PaymentDTO {
+    private Integer id;
+    private Integer invoiceId;
+    private BigDecimal amount;
+    private LocalDate paymentDate;
+    private String status;
+}
