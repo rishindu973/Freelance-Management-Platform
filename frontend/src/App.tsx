6daf9558649bd5db1e24d7f677456f84491fe827
rishindu973 <IT24610795@my.sliit.lk>
2026-04-08 21:06:00 +0530

    implement date filtering

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,9 @@ import Clients from "./pages/Clients";
 import Freelancers from "./pages/Freelancers";
 import Projects from "./pages/Projects";
 import ProjectDetail from "./pages/ProjectDetail";
+import Invoices from "./pages/Invoices";
+import Payments from "./pages/Payments";
+import Reports from "./pages/Reports";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import VerifyEmail from "./pages/VerifyEmail";
@@ -48,6 +51,9 @@ const App = () => (
                 <Route path="/freelancers" element={<Freelancers />} />
                 <Route path="/projects" element={<Projects />} />
                 <Route path="/projects/:id" element={<ProjectDetail />} />
+                <Route path="/invoices" element={<Invoices />} />
+                <Route path="/payments" element={<Payments />} />
+                <Route path="/reports" element={<Reports />} />
               </Route>
             </Route>
 
