6613955ed67a03763a8fcb023091459b9f309ce3
R|cK <IT24610795@my.sliit.lk>
2026-04-16 03:42:20 +0530

    Report initial setup
    * entity classes creation for report
    
    * repository creation and mapping report with details

---
+++ backend/freelancePM/src/main/java/com/freelance/freelancepm/entity/Report.java
@@ -0,0 +1,63 @@
+package com.freelance.freelancepm.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import org.hibernate.annotations.CreationTimestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "reports")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Report {
+
+    public enum ReportType {
+        WEEKLY, MONTHLY, ANNUALY
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    private ReportType type;
+
+    @CreationTimestamp
+    @Column(name = "generated_on", updatable = false)
+    private LocalDateTime generatedOn;
+
+    @OneToMany(mappedBy = "report", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<ReportDetail> details = new ArrayList<>();
+
+    private BigDecimal totalIncome;
+    private BigDecimal totalExpense;
+    private BigDecimal totalProfit;
+
+    public void addDetail(ReportDetail detail) {
+        details.add(detail);
+        detail.setReport(this);
+    }
+
+    public void removeDetail(ReportDetail detail) {
+        details.remove(detail);
+        detail.setReport(null);
+    }
+
+    public void syncTotals() {
+        this.totalIncome = details.stream()
+                .map(ReportDetail::getIncome)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        this.totalExpense = details.stream()
+                .map(ReportDetail::getExpense)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        this.totalProfit = this.totalIncome.subtract(this.totalExpense);
+    }
+}
